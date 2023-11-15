2d5a5f245c7b61ce03db7ccbf1c5e9ffdb386d4b
hiteshvats <54782932+hiteshvats@users.noreply.github.com>
2023-11-14 17:00:02 -0800

    new

--- server.js
+++ server.js
@@ -12,4 +12,27 @@ const storage = multer.diskStorage({
         const folderPath = path.join(__dirname, 'uploads', city);
 
         if (!fs.existsSync(folderPath)) {
-            fs
+            fs.mkdirSync(folderPath, { recursive: true });
+        }
+
+        cb(null, folderPath);
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle the image upload endpoint
+app.post('/uploadImage/:city', upload.single('image'), (req, res) => {
+    res.json({ success: true, message: 'Image uploaded successfully!' });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
