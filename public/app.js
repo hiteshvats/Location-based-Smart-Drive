4570727f2babfda1132ab201c23f4c40d6933f40
umeshgain <umesh.gain@gmail.com>
2023-11-14 16:38:41 -0800

    Update app.js

--- public/app.js
+++ public/app.js
@@ -36,6 +36,19 @@ document.addEventListener('DOMContentLoaded', () => {
     function reverseGeocode(latitude, longitude) {
         // Perform reverse geocoding to get the city name based on the coordinates.
         // For simplicity, we'll use a placeholder "City".
+        const apiKey = 'AIzaSyAu7uQCkbV8pYN5chS-DXMuSsReWc1Btc8';
+
+        const geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ lat: latitude, lng: longitude }, (results, status) => {
+            if (status === 'OK') {
+            const city = results[0].address_components.find(component => component.types.includes('locality')).long_name;
+            currentCity = city;
+            alert(`Location set to: ${city}`);
+  } else {
+    console.error('Error geocoding:', status);
+  }
+});
+
         const city = 'City';
         currentCity = city;
         alert(`Location set to: ${city}`);
